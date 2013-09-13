0492f1ad8bf68a6ecad4ab8d8a9a58ab2c5582be
Matthieu Guillemot <mguillemot@npng.org>
2013-09-13 11:49:34 +0900

    fix auth_cookie initializer when not running the app for real

--- config/initializers/auth_cookie.rb
+++ config/initializers/auth_cookie.rb
@@ -2,11 +2,16 @@
 
 require 'base64'
 
-AUTH_COOKIE_SECRET = if Rails.env.production?
-                       ENV['AUTH_COOKIE_SECRET']
-                     else
-                       'd14ca6ee9268c7470be02f14e72428ed005d5343ca0416cfb585764eb10d4fa3c6b49f88f1e8e0fcf75d349f96bd918af9aa1a49a99b046e2c077bb8e294cf5b'
-                     end
+module Gos
+  AUTH_COOKIE_SECRET = if Rails.env.production?
+                         ENV['AUTH_COOKIE_SECRET']
+                       else
+                         'd14ca6ee9268c7470be02f14e72428ed005d5343ca0416cfb585764eb10d4fa3c6b49f88f1e8e0fcf75d349f96bd918af9aa1a49a99b046e2c077bb8e294cf5b'
+                       end
 
-key = ::Base64.decode64(AUTH_COOKIE_SECRET)
-$authCookieEncryptor = ActiveSupport::MessageEncryptor.new(key)
+  # Note: AUTH_COOKIE_SECRET is not available when precompiling assets
+  if AUTH_COOKIE_SECRET
+    key = ::Base64.decode64(AUTH_COOKIE_SECRET)
+    $authCookieEncryptor = ActiveSupport::MessageEncryptor.new(key)
+  end
+end
